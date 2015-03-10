de6fc55aed8e1e82908e1f367161760445d939ef
JonathanCollaud <jonathan.collaud@epfl.ch>
2015-03-10 11:20:17 +0100

    Class main

--- src/ch/epfl/infovisu/Jeu_realite_augmentee.java
+++ src/ch/epfl/infovisu/Jeu_realite_augmentee.java
@@ -1,11 +1,11 @@
 package ch.epfl.infovisu;
+
 import processing.core.PApplet;
 
 public class Jeu_realite_augmentee {
 
 	public static void main(String[] args) {
-
-		PApplet.main(new String[] { "--present", "Plate"});
+		PApplet.main(new String[] { "--present", "ch.epfl.infovisu.Plate" });
 	}
 
 }
